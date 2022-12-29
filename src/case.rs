a2ea046fae0d59ff7895668cfa8b10b87b4e0101
Haf <haf@protonmail.ch>
2022-12-29 07:59:25 +0100

    impl display for case

--- src/case.rs
+++ src/case.rs
@@ -1,4 +1,5 @@
 use strum_macros::EnumIter;
+use std::fmt::{Formatter, Display, Result as FormatResult};
 
 #[derive(Debug, EnumIter, PartialEq, Eq, Hash, Clone)]
 pub enum Case {
@@ -10,4 +11,17 @@ pub enum Case {
     CamelCase,
     SnakeCase,
     KebabCase,
+}
+
+// TODO: Test
+impl Display for Case {
+    fn fmt(&self, f: &mut Formatter) -> FormatResult {
+        write!(f, "{}", match self {
+            Case::PascalCase => "PascalCase",
+            Case::ShoutingSnakeCase => "SHOUTING_SNAKE_CASE",
+            Case::CamelCase => "camelCase",
+            Case::SnakeCase => "snake_case",
+            Case::KebabCase => "kebab-case",
+        })
+    }
 }

86dd83494208a646c3b75d9ec7e6f5e4c29040fa
Haf <haf@protonmail.ch>
2022-12-29 07:55:06 +0100

    added some args

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,22 @@ fn main() {
                     .about("detects used cases in an input")
                     .arg(arg!(-f --file [FILE] "from file"))
                     .arg(arg!(-i --stdin "from stdin"))
+                    .arg(arg!(-m --main "detect only the main case"))
+                    .arg(arg!(-a --all "detect all cases with their frequencies"))
                     .group(
+                        // TODO: Default of stdin
                         ArgGroup::new("input")
                                   .args(["file", "stdin"])
                                   .required(true) 
+                        
                     )
+                    .group(
+                        // TODO: Default of main
+                        ArgGroup::new("output")
+                                  .args(["main", "all"])
+                                  .required(true)
+                    )
+                    
                 ) 
         .get_matches()
         ;
